0543f44dfcbf3d4f7db4b091d798ed1e3ee43a1b
Louis Gabrie <louisgabrie1450@gmail.com>
2018-04-18 15:30:07 +0000

    I added pictures of projects, created the projects page, and changed the UI.

--- js/script.js
+++ js/script.js
@@ -1,3 +1,6 @@
+// $ global fix 
+/* global $ */
+
 //Unit 4 Portfolio with Object
 // Class 1 
     // use JavaScript to create a simple object representation of your portfolio
@@ -33,3 +36,37 @@
 
 
 // Add your click handler below to update your like counters when the like buttons are clicked. 
+
+
+// function reSizing() { 
+//      if($("jumbtron").class("col-sm-4")) { 
+//           $("jumbotron").css("height","900px");
+//      } 
+// } 
+
+$(window).scroll(function() {
+  if ($(document).scrollTop() > 70) {
+    $('header').addClass('shrink');
+  } else {
+    $('header').removeClass('shrink');
+  }
+});
+
+(function() {
+  'use strict';
+  window.addEventListener('load', function() {
+    // Fetch all the forms we want to apply custom Bootstrap validation styles to
+    var forms = document.getElementsByClassName('needs-validation');
+    // Loop over them and prevent submission
+    var validation = Array.prototype.filter.call(forms, function(form) {
+      form.addEventListener('submit', function(event) {
+        if (form.checkValidity() === false) {
+          event.preventDefault();
+          event.stopPropagation();
+        }
+        form.classList.add('was-validated');
+      }, false);
+    });
+  }, false);
+})();
+
